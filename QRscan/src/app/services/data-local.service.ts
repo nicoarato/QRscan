25b89a13e55bb3a03cca60be87fff4f16d9b3724
Nico Arato <nico.arato@gmail.com>
2020-07-30 20:48:30 -0300

    Servicio para manejar el localstorage de escaneos.

---
+++ QRscan/src/app/services/data-local.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { Registro } from '../models/registro.model';
+import { Storage } from '@ionic/storage';
+import { NavController } from '@ionic/angular';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataLocalService {
+
+  guardados: Registro[] = [];
+
+  constructor( private storage: Storage,
+               private navController: NavController,
+               private iab: InAppBrowser) {
+
+    this.cargarStorage();
+
+   }
+
+   async cargarStorage() {
+     this.guardados = (await this.storage.get('registros')) || [];
+   }
+
+  async guardarRegistro(format: string, text: string) {
+
+    await this.cargarStorage();
+
+    const nuevoRegistro = new Registro(format, text);
+    this.guardados.unshift(nuevoRegistro);
+
+    this.storage.set('registros', this.guardados);
+    console.log(this.guardados);
+
+    this.abrirRegistro(nuevoRegistro);
+
+  }
+
+  abrirRegistro(registro: Registro) {
+
+    // navegacion forzada
+    this.navController.navigateForward('/tabs/tab2');
+
+    switch ( registro.type ) {
+      case 'http':
+        // abrir navegador
+        this.iab.create(registro.text, '_system');
+    }
+  }
+
+}
